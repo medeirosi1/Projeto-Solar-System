c7efa507703a024f5019c77b8e054473f108794b
Thiago Medeiros (Win) <medeiroshs@hotmail.com>
2022-02-18 18:06:50 -0300

    Requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Mission extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions.map((missao, i) => {
+            const { name, year, country, destination } = missao;
+            return (<MissionCard
+              key={ i }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />);
+          })
+        }
       </div>
     );
   }
